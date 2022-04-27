9852c3108b54772456846f897a1c1b63f8e2cf9e
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-27 11:58:27 +0530

    search params

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,14 +1,34 @@
-import { NavLink, Link, Outlet } from 'react-router-dom';
+import { Outlet, NavLink, Link, useSearchParams } from 'react-router-dom';
 import { getInvoices } from "../data";
 
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <div style={{ display: "flex" }}>
       <nav style={{ borderRight: "solid 1px", padding: "1rem", }}>
 
-        {invoices.map((invoice) => (
+      <input
+          value={searchParams.get("filter") || ""}
+          onChange={(event) => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+
+        {invoices
+        .filter((invoice) => {
+          let filter = searchParams.get("filter");
+          if (!filter) return true;
+          let name = invoice.name.toLowerCase();
+          return name.startsWith(filter.toLowerCase());
+        })
+        .map((invoice) => (
           <NavLink
           style={({ isActive }) => {
             return {
@@ -23,6 +43,7 @@ export default function Invoices() {
             {invoice.name}
           </NavLink>
         ))}
+
       </nav>
       <Outlet />
     </div>
