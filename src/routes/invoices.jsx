9b8126126d126a3f516a06efaeaacb7c2fc3613d
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-27 11:45:07 +0530

    active links

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Link, Outlet } from 'react-router-dom';
 import { getInvoices } from "../data";
 
 export default function Invoices() {
@@ -9,13 +9,19 @@ export default function Invoices() {
       <nav style={{ borderRight: "solid 1px", padding: "1rem", }}>
 
         {invoices.map((invoice) => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
+          <NavLink
+          style={({ isActive }) => {
+            return {
+              display: "block",
+              margin: "1rem 0",
+              color: isActive ? "red" : "blue",
+            };
+          }}
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
             {invoice.name}
-          </Link>
+          </NavLink>
         ))}
       </nav>
       <Outlet />
